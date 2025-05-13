08d0b3b8bc8d4ad7a5f01126080ca5e5e1b8bc76
Yogesh Navi <yogeshnavi084@gmail.com>
2025-05-14 02:43:57 +0530

    added db api

--- backend/routes/users.js
+++ backend/routes/users.js
@@ -1,8 +1,15 @@
 var express = require('express');
 var router = express.Router();
+var User = require("../model/user");
+var {connection} = require("../model/connection")
 
 /* GET users listing. */
-router.get('/', function(req, res, next) {
+router.get('/', async function(req, res, next) {
+  await connection();
+  const user = new User({username:"void",name:"parent1",password:"passs"});
+  await user.save().then(()=>{
+    console.log("User saved!");
+  })
   res.send('respond with a resource');
 });
 
