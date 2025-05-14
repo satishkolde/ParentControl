229df8fb508caa5ec11cca3fdd8a2f0818e7f061
Yogesh Navi <yogeshnavi084@gmail.com>
2025-05-14 23:30:58 +0530

    added

--- backend/app.js
+++ backend/app.js
@@ -7,7 +7,7 @@ var dotenv = require('dotenv');
 
 dotenv.config();
 
-var indexRouter = require('./routes/index');
+var keyloggerRouter = require('./routes/keylogger');
 var usersRouter = require('./routes/users');
 
 var app = express();
@@ -18,7 +18,7 @@ app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.use('/', indexRouter);
+app.use('/', keyloggerRouter);
 app.use('/users', usersRouter);
 
 // catch 404 and forward to error handler
