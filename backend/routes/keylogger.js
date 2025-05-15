43d6280493f31f359b5f518a65d9a6f1e8440aea
satishkolde <satishkolde335@gmail.com>
2025-05-15 10:33:17 +0530

    Keylogger Updated

--- backend/routes/keylogger.js
+++ backend/routes/keylogger.js
@@ -5,6 +5,27 @@ const axios = require('axios');
 const clients = []
 const Alert = require('../model/alerts');
 const User = require("../model/user");
+const {uniqueNamesGenerator,adjectives,animals,colors} = require("unique-names-generator");
+
+async function generateUniqueDeviceName(){
+    let devicename;
+    let exists = true;
+    do{
+      devicename = uniqueNamesGenerator({
+        dictionaries: [adjectives, colors, animals],
+        separator: '_',
+        length: 3,
+        style: 'lowerCase'
+      });
+      const user = await User.findOne({
+        device: { $elemMatch: { device_name: devicename } }
+      });
+      if(!user){
+        exists = false;
+      }
+    }while(exists)
+    return devicename;
+}
 
 /* GET home page. */
 router.get('/stream', (req, res) => {
@@ -49,6 +70,16 @@ router.post('/event', async (req, res) => {
   }
 }); 
 
+router.get("/get_unique_id", async (req,res) =>{
 
+  try{
+    const device_name = await generateUniqueDeviceName();
+    res.status(200).json({devicename:device_name});
+  }catch(err){
+    console.log(err);
+    res.status(500).json({message:"Server error!"});
+  }
+  
+})
 
 module.exports = router;
