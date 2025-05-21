a30c06de0620da70ca526a06f553d6c3bb2ee761
prathameshdevaramani <devaramaniprathamesh12@gmail.com>
2025-05-22 01:58:21 +0530

    changed button type

--- frontend/app/login/page.js
+++ frontend/app/login/page.js
@@ -67,9 +67,9 @@ export default function Login() {
                   <div className="text-center mt-6">
                     <button
                       className="bg-lightBlue-500 text-white active:bg-lightBlue-600 text-sm font-bold uppercase px-6 py-3 rounded shadow hover:shadow-lg outline-none focus:outline-none mr-1 mb-1 w-full ease-linear transition-all duration-150"
-                      type="button"
+                      type="submit"
                     >
-                      Sign In
+                      Login
                     </button>
                   </div>
                 </form>
